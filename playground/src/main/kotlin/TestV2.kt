fe9911006faa5b742770fc4268e1a06a19c1a929
dector <dector9@gmail.com>
2018-10-30 13:24:27 +0200

    Simplify test with api

--- playground/src/main/kotlin/TestV2.kt
+++ playground/src/main/kotlin/TestV2.kt
@@ -1,15 +1,17 @@
 import io.github.dector.krokus.api.rotateX
 import io.github.dector.krokus.api.rotateY
-import io.github.dector.krokus.core.geometry.*
-import io.github.dector.krokus.core.space.v
+import io.github.dector.krokus.core.geometry.cube
+import io.github.dector.krokus.core.geometry.cylinder
+import io.github.dector.krokus.core.geometry.minus
+import io.github.dector.krokus.core.geometry.plus
 import io.github.dector.krokus.samples.utils.exportGeometry
 
 
 fun main(args: Array<String>) {
     exportGeometry("playground") {
-        val base = ShapeGeometry(Cube(size = v(10, 10, 10)))
+        val base = cube(10)
 
-        val hole = ShapeGeometry(Cylinder(height = 10.0 + 1, radius = Cylinder.Radius(3.0, 3.0)))
+        val hole = cylinder(height = 10 + 1, radius = 3)
         base - hole.let { it + it.rotateX(90) + it.rotateY(90) }
     }
 }
