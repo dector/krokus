070a3cfb98604852aba35ab1c4423e150c3633cf
dector <dector9@gmail.com>
2018-11-02 14:07:16 +0200

    Power supply shell

--- playground/src/main/kotlin/io/github/dector/krokus/samples/PowerSupply.kt
+++ playground/src/main/kotlin/io/github/dector/krokus/samples/PowerSupply.kt
@@ -1,6 +1,7 @@
 package io.github.dector.krokus.samples
 
 import io.github.dector.krokus.api.moveBy
+import io.github.dector.krokus.api.rotateAtX
 import io.github.dector.krokus.core.geometry.*
 import io.github.dector.krokus.core.space.div
 import io.github.dector.krokus.samples.utils.exportGeometry
@@ -13,29 +14,78 @@ fun main(args: Array<String>) {
         val innerDeep = 40
         val thickness = 2
 
-        val shell = cube(innerWidth, innerDeep, innerHeight).let {
-            it.resizeBy(2 * thickness).uncenter() - it.resizeBy(y = thickness).uncenter().moveBy(thickness)
+        val shell = cube(innerWidth, innerDeep, innerHeight).run {
+            resizeBy(2 * thickness).uncenter() - resizeBy(y = thickness).uncenter().moveBy(thickness)
         }
 
-        val indicatorCut = cube(45, thickness, 26).uncenter().let {
+        val indicatorCut = cube(45, thickness, 26).uncenter().run {
             val ref = shell.bounds()
-            it.moveBy(
-                x = ref.center().x - it.bounds().center().x,
+            moveBy(
+                x = ref.center().x - bounds().center().x,
                 y = ref.from.y,
-                z = ref.center().z - it.bounds().center().z
+                z = ref.center().z - bounds().center().z
             )
         }
 
-        val buttonCut = cube(18, thickness, 12).uncenter().let {
+        val buttonCut = cube(18, thickness, 12).uncenter().run {
             val ref = shell.bounds()
-            it.moveBy(
-                x = ref.center().x - it.bounds().center().x,
+            moveBy(
+                x = ref.center().x - bounds().center().x,
                 y = ref.from.y,
                 z = ref.from.z + (ref.size().z - indicatorCut.bounds().size().z) / 4
-            ).moveBy(z = -it.bounds().size().z / 2)
+            ).moveBy(z = -bounds().size().z / 2)
         }
 
-        shell - indicatorCut - buttonCut
+        val voltageCut = cylinder(thickness + 1, radius = 4).run {
+            val ref = shell.bounds()
+            moveBy(
+                x = ref.size().x - (ref.size().x - indicatorCut.bounds().size().x) / 4,
+                y = shape.height / 2,
+                z = ref.size().z / 2
+            ).rotateAtX(-90)
+        }
+
+        val currentCut = cylinder(thickness + 1, radius = 3.5).run {
+            val ref = shell.bounds()
+            moveBy(
+                x = (ref.size().x - indicatorCut.bounds().size().x) / 4,
+                y = shape.height / 2,
+                z = ref.size().z / 2
+            ).rotateAtX(-90)
+        }
+
+        val outputCut = cylinder(thickness, radius = 5).run {
+            val ref = shell.bounds()
+            moveBy(
+                x = 2 * shape.radius.bottom,//ref.size().x / 5,
+                y = ref.size().y - 2 * shape.radius.bottom,//ref.size().y / 5
+                z = shape.height / 2
+            )
+        }
+
+        val inputCut = cylinder(thickness, radius = 5).run {
+            val ref = shell.bounds()
+            moveBy(
+                x = ref.size().x - 2 * shape.radius.bottom,
+                y = ref.size().y - 2 * shape.radius.bottom,
+                z = ref.size().z - shape.height / 2
+            )
+        }
+
+        val allCuts = listOf(
+            indicatorCut,
+            buttonCut,
+            voltageCut,
+            currentCut,
+            outputCut,
+            inputCut
+        ).union()
+
+        val boardVitamin = cube(52, 27, 13)
+
+        val vitamins = listOf(boardVitamin).union()
+
+        shell - allCuts //+ vitamins
     }
 }
 
--- samples/PowerSupply.scad
+++ samples/PowerSupply.scad
@@ -1,6 +1,14 @@
 $fs=0.25;$fa=6;
-difference(){difference(){difference(){translate([57,22,34.5])cube([114,44,69],center=true);
+union() {
+difference(){difference(){translate([57,22,34.5])cube([114,44,69],center=true);
 translate([57,23,34.5])cube([110,42,65],center=true);
-}translate([57,1,34.5])cube([45,2,26],center=true);
-}translate([57,1,10.75])cube([18,2,12],center=true);
+}union() {
+translate([57,1,34.5])cube([45,2,26],center=true);
+translate([57,1,10.75])cube([18,2,12],center=true);
+translate([96.75,1.5,34.5])rotate([-90,0,0])cylinder(h=3, r=4, center=true);
+translate([17.25,1.5,34.5])rotate([-90,0,0])cylinder(h=3, r=3.5, center=true);
+translate([10,34,1])cylinder(h=2, r=5, center=true);
+translate([104,34,68])cylinder(h=2, r=5, center=true);
+}
+}cube([52,27,13],center=true);
 }
