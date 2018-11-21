33baff75888b9590034542abce4313f5a25fe40f
dector <dector9@gmail.com>
2018-11-22 01:07:48 +0200

    Probably, way to go with properties
    Still don't like `x.ref = ...` but seems like
    the best compromise for me now

--- core/src/main/kotlin/io/github/dector/krokus/core/properties/models.kt
+++ core/src/main/kotlin/io/github/dector/krokus/core/properties/models.kt
@@ -1,101 +1,44 @@
 package io.github.dector.krokus.core.properties
 
-
 interface Property<T> {
-    fun get(): () -> T
-    fun set(v: () -> T)
+    var ref: () -> T
 
-    fun resolve(): T
-}
-
-//abstract class BaseProperty<T> : Property<T> {
-//
-//    override var valueProvider: () -> T
-//        get() = _valueProvider
-//        set(value) { _valueProvider = value }
-//}
-//
-//data class Scalar<T>(val value: T) : Property<T> {
-//
-//    private var _valueProvider: () -> T = { value }
-//    override var valueProvider: () -> T
-//        get() = _valueProvider
-//        set(value) { _valueProvider = value }
-//
-//    override fun toString() = value.toString()
-//}
-//
-//data class Computable<T>(private var provider: () -> T) : Property<T>() {
-//
-//
-//    override var value
-//        get() = provider()
-//        set(value) =
-//
-//            override
-//
-//    fun toString() = value.toString()
-//}
-
-/*data class CubeNode(
-    val size: Property<Int>
-)
-
-data class MapNode<T>(
-    val str: Property<T>,
-    val map: Property<T>
-)
-
-class BaseProperty<T>(private var provider: () -> T) : Property<T> {
-
-    private var _provider: () ->
-
-    override fun get(): Property<T> {
+    fun set(value: T) {
+        ref = { value }
     }
 
-    override fun set(another: Property<T>) {
+    fun set(value: () -> T) {
+        ref = value
     }
 
-    override fun resolveValue() = value
-}
-
-class ScalarProperty<T>(value: T) : BaseProperty<T>({ value }) {
-
-
-}*/
-
-class PropertyImpl<T>(defaultValue: T) : Property<T> {
-
-    private var _value: () -> T = { defaultValue }
-
-    override fun get(): () -> T {
-        return _value
+    companion object {
+        fun <T> from(value: () -> T): Property<T> = PropertyImpl(value)
+        fun <T> from(value: T): Property<T> = from { value }
     }
-
-    override fun set(v: () -> T) {
-        _value = v
-    }
-
-    override fun resolve() = _value()
 }
 
-data class Node(val name: String, val x: Property<Int> = PropertyImpl(0)) {
+internal class PropertyImpl<T>(override var ref: () -> T) : Property<T>
 
-    override fun toString() = "$name.x -> ${x.resolve()}"
+data class Node(val name: String) {
+    var x: Property<Int> = Property.from(0)
+
+    override fun toString() = "$name.x -> ${x.ref()}"
 }
 
-operator fun Property<Int>.times(v: Int) = { get()() * v }
+operator fun Property<Int>.times(v: Int) = { ref() * v }
 
 fun main() {
-    val cube1 = Node("cube1")
+    val cube1 = Node("cube1").apply {
+        x.set(1)
+    }
     val cube2 = Node("cube2").apply {
-        x.set(cube1.x * 2)
+        x.ref = cube1.x * 2
     }
 
     println(cube1)
     println(cube2)
 
-    cube1.x.set { 5 }
+    cube1.x.set(5)
 
     println(cube1)
     println(cube2)
